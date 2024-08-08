07a58e1b917a85becaf41065c289acb556488090
Luis Nieto <luisenieto@gmail.com>
2024-08-08 20:35:02 -0300

    Prueba inicial

---
+++ src/lib/Principal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lib;
+
+/**
+ *
+ * @author luis
+ */
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+}
