f1ee4a2171b196e976af934b73b4f63d05f5ed3c
Luis Nieto <luis.nieto@coto.com.ar>
2024-09-03 11:50:55 -0300

    Modificación al main

--- src/lib/Principal.java
+++ src/lib/Principal.java
@@ -11,5 +11,6 @@ package lib;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Hola mundo 2");
     }
 }
